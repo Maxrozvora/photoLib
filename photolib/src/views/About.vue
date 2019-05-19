886455ca65b711c137fb7a42b116613c516ec922
Maxrozvora <rozvora.max@gmail.com>
2019-05-19 23:02:29 +0300

    work (About.vue) Добавление информации в контакты

--- photolib/src/views/About.vue
+++ photolib/src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Контакты</h1>
+    <ul class="contact">
+      <li class="contact__item"><b>Телефон:</b> +380982767757</li>
+      <li class="contact__item"><b>Email:</b> +admin@photolib.com</li>
+      <li class="contact__item"><b>Адреса:</b> м.Вінниця вул. Космонавтів 5, кв.40</li>
+    </ul>
+    <iframe width="100%" height="440" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=%D0%92%D1%96%D0%BD%D0%BD%D0%B8%D1%86%D1%8F%2C%20%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D0%BD%D0%B0%D0%B2%D1%82%D1%96%D0%B2%205+(%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%B0)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
   </div>
 </template>
