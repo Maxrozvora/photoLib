2fe5f87c53557f8b36fb3d95a0b5787b31575e14
Maxrozvora <rozvora.max@gmail.com>
2019-05-19 22:34:10 +0300

    work (_libs.sass grid.sass item.sass main.sass Home.vue) Создание сетки для картинок

--- photolib/src/views/Home.vue
+++ photolib/src/views/Home.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="home">
-    <!--<slider-item-->
-            <!--:posts="getTopPosts"-->
-    <!--&gt;</slider-item>-->
-    <div class="post-wrapper">
-      <post-item
-              v-for="(post, index) in posts" :key="index"
-              :post="post"
-      ></post-item>
+    <div class="home">
+        <!--<slider-item-->
+        <!--:posts="getTopPosts"-->
+        <!--&gt;</slider-item>-->
+        <div class="container">
+            <div class="post-wrapper">
+                <post-item
+                        v-for="(post, index) in posts" :key="index"
+                        :post="post"
+                ></post-item>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import PostItem from '@/components/PostItem'
-  // import Slider from '@/components/Slider'
+    // @ is an alias to /src
+    import PostItem from '@/components/PostItem'
+    // import Slider from '@/components/Slider'
 
-  import posts from "../data/posts"
-  import filters from "../data/filters"
+    import posts from "../data/posts"
+    import filters from "../data/filters"
 
-  export default {
-    name: 'home',
-    data (){
-      return {
-        posts,
-        filters,
-      }
-    },
-    components: {
-      'post-item': PostItem,
-      // 'slider-item': Slider
+    export default {
+        name: 'home',
+        data() {
+            return {
+                posts,
+                filters,
+            }
+        },
+        components: {
+            'post-item': PostItem,
+            // 'slider-item': Slider
+        }
     }
-  }
 </script>
