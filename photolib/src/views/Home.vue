9a04a94300ec2d9c5ec7382c7c48361f31751d52
max_rozvora <Vbymfkal1967>
2019-05-20 20:42:19 +0300

    work (slider.sass Home.vue) Добавление слайдера

--- photolib/src/views/Home.vue
+++ photolib/src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
     <div class="home">
-        <!--<slider-item-->
-        <!--:posts="getTopPosts"-->
-        <!--&gt;</slider-item>-->
+        <hooper>
+            <slide v-for="(slide, index) in getSliderItem" :key="index">
+                <div class="slide-item">
+                    <div class="slide-item__description">
+                        <h1 class="slide-item__title">Найкраще фото з категорії: {{slide.category}}</h1>
+                        <div class="slide-item__text">Опис:  {{slide.caption}}</div>
+                        <div class="slide-item__text">Лайки: {{slide.likes}} <i class="fa fa-heart"></i></div>
+                    </div>
+                    <div class="slide-item__image-wrap">
+                        <img :src="slide.postImage" :alt="slide.category" class="slide-item__img">
+                    </div>
+                </div>
+
+            </slide>
+
+
+        </hooper>
         <div class="container">
             <div class="post-wrapper">
                 <post-item
@@ -18,6 +32,8 @@
     // @ is an alias to /src
     import PostItem from '@/components/PostItem'
     // import Slider from '@/components/Slider'
+    import { Hooper, Slide } from 'hooper';
+    import 'hooper/dist/hooper.css';
 
     import posts from "../data/posts"
     import filters from "../data/filters"
@@ -30,9 +46,18 @@
                 filters,
             }
         },
+        computed: {
+            getSliderItem () {
+                return this.posts
+            }
+        },
         components: {
             'post-item': PostItem,
-            // 'slider-item': Slider
+            Hooper,
+            Slide
         }
     }
 </script>
+<styles scoped lang="sass" src="./../styles/sass/slider.sass">
+
+</styles>
