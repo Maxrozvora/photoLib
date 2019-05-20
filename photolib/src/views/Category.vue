c8e06720a19cadeb67bcae0435929c00dad301f4
max_rozvora <Vbymfkal1967>
2019-05-20 21:32:38 +0300

    work (_category.sass Category.vue) Вывод страницы категории

--- photolib/src/views/Category.vue
+++ photolib/src/views/Category.vue
@@ -2,29 +2,42 @@
     <div class="category">
         <div class="pattern pattern-cat-header">
             <div class="cat-header overlay" style="background-image: url('img/blog/electronics/_test.jpg');">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-7">
-                            <div class="cat-header-block">
+                <div class="cat-header-block">
                                 <h1>
-                                    Электроника
+                                    Категория
                                 </h1>
-                                <p>Новости современной электроники - гаджеты, ультрабуки, новые смартфоны и смарт часы, VR и многое другое</p>
+                                <p>описание</p>
                             </div>
-                        </div>
-                    </div>
-                </div>
             </div>
+        </div>
+        <h1>Назва категории</h1>
+        <div class="post-wrapper">
+            <post-item
+                    v-for="(post, index) in posts" :key="index"
+                    :post="post"
+            ></post-item>
         </div>
     </div>
 </template>
 
 <script>
+    import PostItem from '@/components/PostItem'
+
+    import posts from "../data/posts"
     export default {
-        name: "Category"
+        name: "Category",
+        data() {
+            return {
+                posts
+            }
+        },
+        components: {
+            'post-item': PostItem
+        }
     }
+
 </script>
 
-<style scoped>
+<style scoped lang="sass" src="./../styles/sass/_category.sass">
 
 </style>
