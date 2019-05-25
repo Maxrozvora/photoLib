c3907e4fc5ece50a221228a87cf856979c245b7c
Maxrozvora <rozvora.max@gmail.com>
2019-05-25 12:29:25 +0300

    work (Home.vue) Реализация описка по двум критериям

--- photolib/src/views/Home.vue
+++ photolib/src/views/Home.vue
@@ -1,22 +1,5 @@
 <template>
     <div class="home">
-        <hooper>
-            <slide v-for="(slide, index) in getSliderItem" :key="index">
-                <div class="slide-item">
-                    <div class="slide-item__description">
-                        <h1 class="slide-item__title">Найкраще фото з категорії: {{slide.category}}</h1>
-                        <div class="slide-item__text">Опис:  {{slide.caption}}</div>
-                        <div class="slide-item__text">Лайки: {{slide.likes}} <i class="fa fa-heart"></i></div>
-                    </div>
-                    <div class="slide-item__image-wrap">
-                        <img :src="slide.postImage" :alt="slide.category" class="slide-item__img">
-                    </div>
-                </div>
-
-            </slide>
-
-
-        </hooper>
         <div class="container">
             <h1 class="page-title">Найкраші фото</h1>
             <div class="input-group">
