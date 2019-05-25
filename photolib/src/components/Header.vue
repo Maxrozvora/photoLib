963f8a818ef8c936bc00032f9a8133bce0595e27
Maxrozvora <rozvora.max@gmail.com>
2019-05-25 13:14:55 +0300

    work (Header.vue) Удаление формы поиска

--- photolib/src/components/Header.vue
+++ photolib/src/components/Header.vue
@@ -1,19 +1,6 @@
 <template>
     <header class="top-line">
         <router-link :to="'/'" tag="a" class="logo logo-litera">PhotoLib</router-link>
-        <div class="search-wrap">
-            <div class="search" title="Поиск по сату">
-                <div class="fa fa-search"></div>
-            </div>
-            <div class="search-field">
-                <form action="/search.html" id="cse-search-box">
-                    <input type="hidden" name="cx" value="partner-pub-xxx:xxx"/>
-                    <input type="hidden" name="cof" value="FORID:10"/>
-                    <input type="hidden" name="ie" value="UTF-8"/>
-                    <input type="text" name="q" placeholder="Поиск по сайту..."/>
-                </form>
-            </div>
-        </div>
         <div @click="toggleMobileNav" class="mobile-menu-button"><i class="fa fa-bars"></i> Меню</div>
         <nav class="main-menu top-menu" :class="{active: toggleNav}">
             <ul class="main-menu__list">
