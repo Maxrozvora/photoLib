4470273799eb07b6665e985daa01fb478ba2c6a5
Maxrozvora <rozvora.max@gmail.com>
2019-05-25 12:38:35 +0300

    work (Slider.vue slider.sass) Добавление компонента слайдера

---
+++ photolib/src/components/Slider.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <button v-on:click='prevSlide'>Prev</button>
+        <button v-on:click='nextSlide'>Next</button>
+        <button v-on:click='openSlide(3)'>Open slide 3</button>
+
+        <div class='slider js-slider'>
+            <div class="slider__body" v-bind:style='{left: sliderOffsetLeft + "px"}'>
+                <div class="slider__slide js-slide" v-for='slide in sliderList' :style='"background-image: url(" + slide.img + ")"'></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Slider",
+        data () {
+            return {
+                // Всего слайдов
+                sliderAllCount: 0,
+                // Номер активного слайда
+                sliderActive: 1,
+                // Отступ тела со слайдами в контейнере
+                sliderOffsetLeft: 0,
+                // Шаг одного слайда = его длина
+                sliderOffsetStep: 0,
+                // Список изображений
+                sliderList: [
+                    {img: 'https://dummyimage.com/wsvga'},
+                    {img: 'https://dummyimage.com/wsvga'},
+                    {img: 'https://dummyimage.com/wsvga'},
+                    {img: 'https://dummyimage.com/wsvga'}
+                ]
+            }
+        },
+
+        methods: {
+            // Иницилизация слайдера
+            initSlider: function () {
+                // Получаем элементы сладера и его слайдов
+                let sliderBody = this.$el.querySelector('.js-slider')
+                let sliderSlidies = sliderBody.querySelectorAll('.js-slide')
+                // Записываем длину одного слайда для перелистывания
+                this.sliderOffsetStep = sliderBody.clientWidth
+                // Общее количество слайдов для стопов
+                this.sliderAllCount = sliderSlidies.length
+            },
+
+            // Открыть слайд по номеру
+            openSlide: function (id) {
+                if (id > 0 && id <= this.sliderAllCount) {
+                    this.sliderActive = id
+                    // Сдвигаем элемент со слайдами
+                    this.sliderOffsetLeft = -(this.sliderActive * this.sliderOffsetStep - this.sliderOffsetStep)
+                }
+            },
+
+            // Следующий слайд
+            nextSlide: function () {
+                if (this.sliderActive < this.sliderAllCount) {
+                    this.sliderActive += 1
+                    this.openSlide(this.sliderActive)
+                }
+            },
+
+            // Предыдущий слайд
+            prevSlide: function () {
+                if (this.sliderActive > 1) {
+                    this.sliderActive -= 1
+                    this.openSlide(this.sliderActive)
+                }
+            }
+        },
+
+        mounted () {
+            this.initSlider()
+
+            // Перенастройка слайдера при ресайзе окна
+            window.addEventListener('resize', () => {
+                this.initSlider()
+                this.openSlide(this.sliderActive)
+            })
+        }
+    }
+</script>
+
+<style scoped lang="sass" src="../styles/sass/slider.sass">
+
+</style>
