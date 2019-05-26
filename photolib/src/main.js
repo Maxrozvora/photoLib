add9f9d2bd64c8e88dc38e9ad01b9fe70717a93d
Maxrozvora <rozvora.max@gmail.com>
2019-05-26 10:29:38 +0300

    work (main.js package.json) подключение lodash

--- photolib/src/main.js
+++ photolib/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import _ from 'lodash'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  _,
   render: h => h(App)
 }).$mount('#app')
