f357352d4d2c788494c0e627e1c126d63bbe1926
Maxrozvora <rozvora.max@gmail.com>
2019-05-27 12:37:11 +0300

    work (about.sass About.vue) Стилизация страницы контактов

--- photolib/src/views/About.vue
+++ photolib/src/views/About.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="about">
-    <h1>Контакты</h1>
-    <ul class="contact">
-      <li class="contact__item"><b>Телефон:</b> +380982767757</li>
-      <li class="contact__item"><b>Email:</b> +admin@photolib.com</li>
-      <li class="contact__item"><b>Адреса:</b> м.Вінниця вул. Космонавтів 5, кв.40</li>
-    </ul>
+    <div class="about-header-wrapper">
+      <div class="about-header">
+        <h1>Контакты</h1>
+        <ul class="contact">
+          <li class="contact__item"><b>Телефон:</b> +380982767757</li>
+          <li class="contact__item"><b>Email:</b> +admin@photolib.com</li>
+          <li class="contact__item"><b>Адреса:</b> м.Вінниця вул. Космонавтів 5, кв.40</li>
+        </ul>
+      </div>
+    </div>
+
+
     <iframe width="100%" height="440" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=%D0%92%D1%96%D0%BD%D0%BD%D0%B8%D1%86%D1%8F%2C%20%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D0%BD%D0%B0%D0%B2%D1%82%D1%96%D0%B2%205+(%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%B0)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
   </div>
 </template>
+
+<style scoped lang="sass" src="../styles/sass/about.sass">
+
+</style>
